3030e73c1f274bebe16be6f3e187bdcc554cf168
AlexMarchetto <alex.marchetto@etu.univ-grenoble-alpes.fr>
2026-06-02 13:59:51 +0200

    Mise en place du tp3

---
+++ tp3/controllers/FourWheelsCollisionAvoidanceAPI/api/state/RobotMode.java
@@ -0,0 +1,11 @@
+package api.state;
+
+/*
+ * TODO TP3
+ * Completez la liste des modes utilises dans ce TP.
+ * Un mode correspond a une etape du comportement du robot.
+ */
+public enum RobotMode {
+  // Modes du robot ...
+  SEARCH
+}
